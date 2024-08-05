5a31bcb4bc4ab4b9fe61bfe77dd33bf176d1f4bc
isaacyang <isaac.yang@canonical.com>
2024-08-05 19:27:26 +0800

    Add some new feature

--- Get_TW_Stock.py
+++ Get_TW_Stock.py
@@ -30,11 +30,11 @@ class Logger(logging.Logger):
             "%(log_color)s%(asctime)s - %(name)s - %(levelname)s - "
             "%(message)s",
             log_colors={
-                'DEBUG': 'cyan',
-                'INFO': 'light_white',
-                'WARNING': 'yellow',
-                'ERROR': 'red',
-                'CRITICAL': 'bold_red',
+                "DEBUG": "cyan",
+                "INFO": "light_white",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
             },
         )
 
@@ -79,7 +79,7 @@ TPEX_BASE_URL = "http://www.tpex.org.tw/"
 
 # url = urllib.parse.urljoin(TWSE_BASE_URL, "exchangeReport/STOCK_DAY")
 def json_dump(data, file):
-    with open(file, 'w', encoding="utf-8") as f:
+    with open(file, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
@@ -103,7 +103,7 @@ class MutiThread:
             max_workers=self.max_threads
         ) as executor:
             futures = {
-                executor.submit(work_function, item): item for item in items
+                executor.submit(work_function, *item): item for item in items
             }
             results = {}
             failed_items = []
@@ -112,8 +112,8 @@ class MutiThread:
                 item = futures[future]
                 try:
                     result = future.result()
-                    results[item] = result
-                    self.logger.log_debug(f"Task for {item} finished")
+                    results[str(item)] = result
+                    self.logger.log_debug(f"Task for {str(item)} finished")
 
                 except Exception as e:
                     failed_items.append(item)
@@ -225,15 +225,15 @@ class StockCodeMapping:
                 stock_code = stock_dict[code_key]
                 stock_name = stock_dict[name_key]
                 if (
-                    not re.findall(r'(購|售|熊|年|元展)\d+', stock_name)
-                    and not re.findall(r'[A-Z]', stock_code)
-                    and not re.findall(r'0200\d{2}', stock_code)
+                    not re.findall(r"(購|售|熊|年|元展)\d+", stock_name)
+                    and not re.findall(r"[A-Z]", stock_code)
+                    and not re.findall(r"0200\d{2}", stock_code)
                 ):
                     stock_code_mapping[stock_code] = {
-                        'Code': stock_code,
-                        'yfinance_code': f"{stock_code}.{yfinance_suffix}",
-                        'Name': stock_name,
-                        'Type': stock_type,
+                        "Code": stock_code,
+                        "yfinance_code": f"{stock_code}.{yfinance_suffix}",
+                        "Name": stock_name,
+                        "Type": stock_type,
                     }
 
         for market, (
@@ -270,14 +270,168 @@ class StockHistory:
         self.output_folder_path = Path(output_folder)
         self.output_folder_path.mkdir(parents=True, exist_ok=True)
 
-    def get_stock_history(self, yf_stock_number, start=""):
-        ticker = yf.Ticker(yf_stock_number)
-        if start:
-            df = ticker.history(start=start, period="max")
+    def __fetch_stock_history(self, stock_numbers, start):
+        stock_data = {}
+        tickers = yf.Tickers(" ".join(stock_numbers))
+
+        for stock_number in stock_numbers:
+            if start:
+                df = tickers.tickers[stock_number].history(
+                    start=start, period="max"
+                )
+            else:
+                df = tickers.tickers[stock_number].history(period="max")
+            df = df.round(2)
+            stock_data[stock_number] = df
+
+        return stock_data
+
+    def get_stock_history(self, yf_stock_number_list, start=None):
+        self.logger.log_debug(
+            f"Get stock history data for {yf_stock_number_list} "
+            f"start from {start}"
+        )
+
+        if isinstance(yf_stock_number_list, str):
+            yf_stock_number_list = [yf_stock_number_list]
+
+        m_thread = MutiThread()
+        max_group = 100
+        tasks = [
+            (yf_stock_number_list[i : i + max_group], start)
+            for i in range(0, len(yf_stock_number_list), max_group)
+        ]
+
+        results = m_thread.run_multithreaded(self.__fetch_stock_history, tasks)
+
+        stock_data = {}
+        for result in results["result"].values():
+            stock_data.update(result)
+
+        if not results["status"]:
+            self.logger.log_error(
+                f"Failed to get stock history data: {results['fail_items']}"
+            )
+
+        return stock_data
+
+    # No one use this function
+    def save_stocks_df_to_csv(self, stock_number_list, df_list):
+        if isinstance(stock_number_list, str):
+            stock_number_list = [stock_number_list]
+        if isinstance(df_list, pd.DataFrame):
+            df_list = [df_list]
+
+        if len(stock_number_list) != len(df_list):
+            self.logger.log_error("stock_number_list and df_list not match")
+            return
+
+        for stock_number, df in zip(stock_number_list, df_list):
+            if ".TWO" in stock_number:
+                file_name = stock_number.replace(".TWO", "") + ".csv"
+            elif ".TW" in stock_number:
+                file_name = stock_number.replace(".TW", "") + ".csv"
+
+            file_full_path = self.output_folder_path / file_name
+            if len(df.index) == 0:
+                self.logger.log_warning(f"{stock_number} has no data")
+            df.to_csv(file_full_path)
+
+    # No one use this function
+    def read_stock_df_from_csv(self, stock_number_list):
+        if isinstance(stock_number_list, str):
+            stock_number_list = [stock_number_list]
+
+        stock_data = {}
+        for stock_number in stock_number_list:
+            if ".TWO" in stock_number:
+                file_name = stock_number.replace(".TWO", "") + ".csv"
+            elif ".TW" in stock_number:
+                file_name = stock_number.replace(".TW", "") + ".csv"
+
+            file_full_path = self.output_folder_path / file_name
+            if file_full_path.is_file():
+                df = pd.read_csv(file_full_path, index_col="Date")
+                stock_data[stock_number] = df
+            else:
+                self.logger.log_info(f"{stock_number} has no data")
+                stock_data[stock_number] = pd.DataFrame()
+        return stock_data
+
+    # No one use this function
+    def get_stock_df_from_web(self, stock_number_list):
+        if isinstance(stock_number_list, str):
+            stock_number_list = [stock_number_list]
+
+        stock_data = {}
+        self.get_stock_df_from_web(stock_number_list)
+        return
+
+    # No OLD one use this function
+    def save_stock_df_to_csv(self, stock_number, df_new):
+        if ".TWO" in stock_number:
+            file_name = stock_number.replace(".TWO", "") + ".csv"
+        elif ".TW" in stock_number:
+            file_name = stock_number.replace(".TW", "") + ".csv"
+
+        file_full_path = self.output_folder_path / file_name
+        if file_full_path.is_file():
+            df_exist = pd.read_csv(file_full_path, index_col="Date")
+            last_date_in_csv = df_exist.index[-1].split(" ")[0]
+            today_date = datetime.datetime.today()
+            last_open_time = smt.last_open_time(today_date).strftime(
+                "%Y-%m-%d"
+            )
+            if (
+                last_date_in_csv == last_open_time
+                and last_date_in_csv != today_date.strftime("%Y-%m-%d")
+            ):
+                return
+            if last_date_in_csv == today_date.strftime("%Y-%m-%d"):
+                df_exist = df_exist.iloc[:-1]
+            else:
+                df_new = df_new.iloc[1:]
+            df = pd.concat([df_exist, df_new]).drop_duplicates()
         else:
-            df = ticker.history(period="max")
-        df = df.round(2)
-        return df
+            df = df_new
+        if len(df.index) == 0:
+            self.logger.log_warning(f"{stock_number} has no data")
+            return
+
+        df.to_csv(file_full_path)
+
+    def save_stock_history_list(self, stock_number_list):
+        df_dict = self.get_stock_history(stock_number_list)
+        for stock_number, df_new in df_dict.items():
+            if ".TWO" in stock_number:
+                file_name = stock_number.replace(".TWO", "") + ".csv"
+            elif ".TW" in stock_number:
+                file_name = stock_number.replace(".TW", "") + ".csv"
+            file_full_path = self.output_folder_path / file_name
+            if file_full_path.is_file():
+                df_exist = pd.read_csv(file_full_path, index_col="Date")
+                last_date_in_csv = df_exist.index[-1].split(" ")[0]
+                today_date = datetime.datetime.today()
+                last_open_time = smt.last_open_time(today_date).strftime(
+                    "%Y-%m-%d"
+                )
+                if (
+                    last_date_in_csv == last_open_time
+                    and last_date_in_csv != today_date.strftime("%Y-%m-%d")
+                ):
+
+                    continue
+
+                df = pd.concat(
+                    [df_exist[~df_exist.index.isin(df_new.index)], df_new]
+                ).drop_duplicates()
+
+            else:
+                df = self.df_new
+            if len(df.index) == 0:
+                self.logger.log_warning(f"{stock_number} has no data")
+                return
+            df.to_csv(file_full_path)
 
     def save_stock_history(self, stock_number):
         if ".TWO" in stock_number:
@@ -287,19 +441,22 @@ class StockHistory:
         file_full_path = self.output_folder_path / file_name
 
         if file_full_path.is_file():
-            df_exist = pd.read_csv(file_full_path, index_col='Date')
-            last_date_in_csv = df_exist.index[-1].split(' ')[0]
+            df_exist = pd.read_csv(file_full_path, index_col="Date")
+            last_date_in_csv = df_exist.index[-1].split(" ")[0]
             today_date = datetime.datetime.today()
-            last_open_time = smt.last_open_time(
-                today_date
-            ).strftime('%Y-%m-%d')
-            if last_date_in_csv == last_open_time and last_date_in_csv != today_date.strftime('%Y-%m-%d'):
+            last_open_time = smt.last_open_time(today_date).strftime(
+                "%Y-%m-%d"
+            )
+            if (
+                last_date_in_csv == last_open_time
+                and last_date_in_csv != today_date.strftime("%Y-%m-%d")
+            ):
                 return
 
             df_new = self.get_stock_history(
                 stock_number, start=last_date_in_csv
-            )
-            if last_date_in_csv ==today_date.strftime('%Y-%m-%d'):
+            )[stock_number]
+            if last_date_in_csv == today_date.strftime("%Y-%m-%d"):
                 df_exist = df_exist.iloc[:-1]
             else:
                 df_new = df_new.iloc[1:]
@@ -319,13 +476,15 @@ def main():
     stock_history = StockHistory("./DataBase")
     stock_code_mapping = json.load(open(file, "r"))
     stock_numbers = [
-        stock['yfinance_code'] for stock in stock_code_mapping.values()
+        stock["yfinance_code"] for stock in stock_code_mapping.values()
     ]
+    # stock_numbers = stock_numbers[:10]
     m_thread = MutiThread(max_threads=0)
+    tasks = [(stock_number,) for stock_number in stock_numbers]
     result = m_thread.run_multithreaded(
-        stock_history.save_stock_history, stock_numbers
+        stock_history.save_stock_history, tasks
     )
-    if result['status']:
+    if result["status"]:
         log.log_info("All stock history data saved successfully")
     else:
         log.log_error(
